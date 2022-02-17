38067cd5e43a5ee1f21d470ee1f5443b65732b61
Aditya Wijaya <85105330+aditya311200@users.noreply.github.com>
2022-02-17 14:50:37 +0700

    commit call action

--- app/src/main/java/com/example/adv160419051week2/MainFragment.kt
+++ app/src/main/java/com/example/adv160419051week2/MainFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.android.material.navigation.NavigationBarItemView
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +16,13 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnStart.setOnClickListener {
+            val action = MainFragmentDirections.actionGameFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
