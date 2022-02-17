e0e7821911718120232a93ed55de1ba8b0bddade
Aditya Wijaya <85105330+aditya311200@users.noreply.github.com>
2022-02-17 18:03:23 +0700

    commit class_work_complete

--- app/src/main/java/com/example/adv160419051week2/MainActivity.kt
+++ app/src/main/java/com/example/adv160419051week2/MainActivity.kt
@@ -7,9 +7,16 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()
     }
 }
